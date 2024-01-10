3050addef35b1177711394385bdff3bd67343d83
n-kev1n <kevin_@tuta.io>
2024-01-10 20:30:20 +0100

    removed old logic and imported new classes to handle most functionalities

--- src/main/java/pl/foodrating/Main.java
+++ src/main/java/pl/foodrating/Main.java
@@ -1,46 +1,12 @@
 package pl.foodrating;
 
-import java.util.InputMismatchException;
-import java.util.Scanner;
-
-import pl.foodrating.utils.Menu;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("\n======================================");
-        System.out.println("Welcome to Food outlets rating system");
-
-        boolean appState = true;
-
-        // Creating Menu
-        Menu menu = new Menu();
-        int choice;
-
-        menu.addOption("Load data from file");
-        menu.addOption("View Restaurants, Take-away shops");
-        menu.addOption("Add Restaurants, Take-away shops");
-        menu.addOption("Add rating");
-        menu.addOption("Quit application");
-
-        Scanner scanner = new Scanner(System.in);
-
-        do {
-            menu.display();
-            try {
-                choice = scanner.nextInt();
-                if (choice == 5) {
-                    appState = false;
-                }
-            } catch (InputMismatchException e) {
-                appState = false;
-                System.err.println("Invalid input!!");
-                System.err.println("Please use numbers to indicate your choice\n");
-
-            } catch (Exception e) {
-                System.err.println(e);
-            }
-
-        } while (appState);
-        scanner.close();
+        List<FoodOutlet> foodOutlets = DataService.loadSampleData("sample_data.txt");
+        FoodOutletManager outletManager = new FoodOutletManager(foodOutlets);
+        Menu menu = new Menu(outletManager);
+        menu.displayMenu();
     }
 }
